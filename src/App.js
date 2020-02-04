7e18875f7ef0c0857eb2104338de2c097f952ec7
Beth Urban <beth@mosaicstg.com>
2020-02-04 16:47:48 -0500

    Set up basic fetching and display of WP API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>NationBuilder Reports</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      people: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost/wp_nbreports/wp-json/nbtools/v1/nbpeople?per_page=100')
+      .then(resp => resp.json())
+      .then(data => this.setState((prevState, props) => {
+        return { people:data};
+      }))
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>NationBuilder Reports</h1>
+        </header>
+
+        <div>
+          <p>Name: {this.state.people.key_name}</p>
+          <p>Data: {this.state.people.data}</p>
+        </div>
+      </div>
+
+    );
+  }
+
 }
 
 export default App;
