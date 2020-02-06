7819487d62bb669db35384ab87c3e0b5ca1dfbfb
Beth Urban <beth@mosaicstg.com>
2020-02-06 11:21:26 -0500

    Add react-bootstrap and bootstrap. Load nations in App and add simple dropdown.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 import './App.css';
 
 class App extends Component {
@@ -6,16 +7,22 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      people: {}
+      nations: {}
     }
   }
 
+  //componentDidMount() {
+  //  fetch('http://localhost/wp_nbreports/wp-json/nbtools/v1/nbpeople?per_page=100')
+  //    .then(resp => resp.json())
+  //    .then(data => this.setState((prevState, props) => {
+  //      return { people: data[0]};
+  //    }))
+  //}
+
   componentDidMount() {
-    fetch('http://localhost/wp_nbreports/wp-json/nbtools/v1/nbpeople?per_page=100')
+    fetch('http://localhost/wp_nbreports/wp-json/wp/v2/nations')
       .then(resp => resp.json())
-      .then(data => this.setState((prevState, props) => {
-        return { people: data[0]};
-      }))
+      .then(data => this.setState({nations: data}))
   }
 
   render () {
@@ -25,10 +32,20 @@ class App extends Component {
           <h1>NationBuilder Reports</h1>
         </header>
 
-        <div>
-          <p>{this.state.people.key_name}</p>
-          <p>{this.state.people.data}</p>
+        <div className="Dropdown">
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-title">
+              Choose a nation
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
+
       </div>
 
     );
