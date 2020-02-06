75d4d1a34c0152a5b06a682e134ff275e535f9b7
Beth Urban <beth@mosaicstg.com>
2020-02-06 13:23:48 -0500

    Add checkboxes for report types

--- src/containers/ReportOptions.js
+++ src/containers/ReportOptions.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 class ReportOptions extends Component {
 
@@ -8,8 +9,18 @@ class ReportOptions extends Component {
 
   render () {
     return (
-      <div>
-        Hello, report options!
+      <div className="report-options">
+        <h3>What types of reports do you need for {this.props.url}?</h3>
+
+        <Form>
+            <div className="checkboxes">
+              <Form.Check type="checkbox" id="people" label="People" />
+              <Form.Check type="checkbox" id="lists" label="Lists" />
+            </div>
+            <div className="submit">
+              <Button type="submit">Pull reports</Button>
+            </div>
+        </Form>
       </div>
     )
   }
