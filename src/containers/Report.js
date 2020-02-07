63e0a6644d5694dc2ed81dd9edb1ff4c2c75a24e
Beth Urban <beth@mosaicstg.com>
2020-02-07 13:48:10 -0500

    Add fetch calls for all API requests without params

--- src/containers/Report.js
+++ src/containers/Report.js
@@ -24,12 +24,43 @@ class Report extends Component {
   }
 
   componentDidMount() {
+
+    if (this.props.reportsWanted.includes("people")) {
+      fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/people?access_token=${this.props.token}` )
+        .then( resp => resp.json() )
+        .then( data => this.setState( { people: data } ) )
+    }
+
+    if (this.props.reportsWanted.includes("people-count")) {
+      fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/people/count?access_token=${this.props.token}` )
+        .then( resp => resp.json() )
+        .then( data => this.setState( { peopleCount: data } ) )
+    }
+
+    if (this.props.reportsWanted.includes("lists")) {
+      fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/lists?access_token=${this.props.token}` )
+        .then( resp => resp.json() )
+        .then( data => this.setState( { lists: data } ) )
+    }
+
+    if (this.props.reportsWanted.includes("tags")) {
+      fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/tags?access_token=${this.props.token}` )
+        .then( resp => resp.json() )
+        .then( data => this.setState( { tags: data } ) )
+    }
+
+    if (this.props.reportsWanted.includes("donations")) {
+      fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/donations?access_token=${this.props.token}` )
+        .then( resp => resp.json() )
+        .then( data => this.setState( { donations: data } ) )
+    }
+
     if (this.props.reportsWanted.includes("paths")) {
       fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/paths?access_token=${this.props.token}` )
         .then( resp => resp.json() )
         .then( data => this.setState( { paths: data } ) )
     }
-
+    
   }
 
   render () {
