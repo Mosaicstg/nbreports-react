d644f5fee75a2714bfa82fdb417ff5afe13a3fda
Beth Urban <beth@mosaicstg.com>
2020-02-07 16:38:49 -0500

    Start adding options fields for API parameters beneath checkboxes

--- src/containers/ApiOptions.js
+++ src/containers/ApiOptions.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Col } from 'react-bootstrap';
 
-function ApiOptions( props) {
+function ApiOptions( props ) {
+
+  let apiForm;
+
+  if (props.id === "contact-single") {
+    apiForm = (
+      <Form><Form.Control size="sm" type="text" id={props.id} placeholder="Person's id" /></Form>
+    )
+  }
+
+  if (props.id === "donations-search") {
+    apiForm = (
+      <Form>
+        <Form.Row>
+          <Col>
+            <Form.Control size="sm" type="text" id={props.id} placeholder="Created since" />
+          </Col>
+          <Col>
+            <Form.Control size="sm" type="text" id={props.id} placeholder="Succeeded since" />
+          </Col>
+          <Col>
+            <Form.Control size="sm" type="text" id={props.id} placeholder="Failed since" />
+          </Col>
+        </Form.Row>
+      </Form>
+    )
+  }
+
+  if (props.id === "events") {
+    apiForm = (
+      <Form><Form.Control size="sm" type="text" id={props.id} placeholder="Site slug" /></Form>
+    )
+  }
+
+  if (props.id === "event-single" || props.id === "event-rsvps") {
+    apiForm = (
+      <Form>
+        <Form.Row>
+          <Col>
+            <Form.Control size="sm" type="text" id={props.id} placeholder="Site slug" />
+          </Col>
+          <Col>
+            <Form.Control size="sm" type="text" id={props.id} placeholder="Event ID" />
+          </Col>
+        </Form.Row>
+      </Form>
+    )
+  }
 
   return (
     <div>
-      API options!
+      { apiForm }
     </div>
   )
 
