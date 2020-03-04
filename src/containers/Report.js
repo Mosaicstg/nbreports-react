49ac1841b7d839680e91daa701bb0fdc4abd02e7
Beth Urban <beth@mosaicstg.com>
2020-03-04 13:22:50 -0500

    Refactor Report

--- src/containers/Report.js
+++ src/containers/Report.js
@@ -28,175 +28,79 @@ class Report extends Component {
   componentDidMount() {
     const arr = [];
 
+    const apiStart = `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/`;
+
+    const apiEnd = `?access_token=${this.props.token}`;
+
     this.props.reportsWanted.forEach( reportWanted => {
+      let reportName = reportWanted.name;
+      let apiEndpoint;
 
-      if (reportWanted.name === "contactSingle") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/people/${reportWanted.params.id}/contacts?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { contactSingle: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'contactSingle') {
+        apiEndpoint = apiStart + `people/${reportWanted.params.id}/contacts` + apiEnd;
       }
 
-      if (reportWanted.name === "donations") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/donations?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { donations: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'donations') {
+        apiEndpoint = apiStart + 'donations' + apiEnd;
       }
 
-      if (reportWanted.name === "donationsSearch") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/donations/search?created_since=${reportWanted.params.createdSince}&succeeded_since=${reportWanted.params.succeededSince}&failed_since=${reportWanted.params.failedSince}?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { donationsSearch: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'donationsSearch') {
+        apiEndpoint = apiStart + `donations/search?created_since=${reportWanted.params.createdSince}&succeeded_since=${reportWanted.params.succeededSince}&failed_since=${reportWanted.params.failedSince}` + apiEnd;
       }
 
-      if (reportWanted.name === "events") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/sites/${reportWanted.params.siteSlug}/pages/events?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { events: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'events') {
+        apiEndpoint = apiStart + `sites/${reportWanted.params.siteSlug}/pages/events` + apiEnd;
       }
 
-      if (reportWanted.name === "eventSingle") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/sites/${reportWanted.params.siteSlug}/pages/events/${reportWanted.params.id}?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.event) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { eventSingle: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'eventSingle') {
+        apiEndpoint = apiStart + `sites/${reportWanted.params.siteSlug}/pages/events/${reportWanted.params.id}` + apiEnd;
       }
 
-      if (reportWanted.name === "eventRsvps") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/sites/${reportWanted.params.siteSlug}/pages/events/${reportWanted.params.id}/rsvps?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { eventRsvps: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'eventRsvps') {
+        apiEndpoint = apiStart + `sites/${reportWanted.params.siteSlug}/pages/events/${reportWanted.params.id}/rsvps` + apiEnd;
       }
 
-      if (reportWanted.name === "lists") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/lists?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { lists: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'lists') {
+        apiEndpoint = apiStart + 'lists' + apiEnd;
       }
 
-      if (reportWanted.name === "listPeople") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/lists/${reportWanted.params.id}/people?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { listPeople: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'listPeople') {
+        apiEndpoint = apiStart + `lists/${reportWanted.params.id}/people` + apiEnd;
       }
 
-      if (reportWanted.name === "membershipSingle") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/people/${reportWanted.params.id}/memberships?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { membershipSingle: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'membershipSingle') {
+        apiEndpoint = apiStart + `people/${reportWanted.params.id}/memberships` + apiEnd;
       }
 
-      if (reportWanted.name === "paths") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/paths?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { paths: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'paths') {
+        apiEndpoint = apiStart + 'paths' + apiEnd;
       }
 
-      if (reportWanted.name === "people") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/people?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { people: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'people') {
+        apiEndpoint = apiStart + 'people' + apiEnd;
       }
 
-      if (reportWanted.name === "peopleCount") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/people/count?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { peopleCount: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'peopleCount') {
+        apiEndpoint = apiStart + 'people/count' + apiEnd;
       }
 
-      if (reportWanted.name === "tags") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/tags?limit=100&access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { tags: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'tags') {
+        apiEndpoint = apiStart + 'tags' + apiEnd;
       }
 
-      if (reportWanted.name === "tagPeople") {
-        fetch( `https://cors-anywhere.herokuapp.com/${this.props.url}/api/v1/tags/${reportWanted.params.tag}/people?access_token=${this.props.token}` )
-          .then( resp => resp.json() )
-          .then( json => arr.concat(json.results) )
-          .then( data => Papa.unparse(data) )
-          .then( csv => {
-            let csvFile = "data:text/csv;charset=utf-8," + csv;
-            this.setState( { tagPeople: csvFile } )
-          })
-          .catch( error => console.log("Error: ", error) )
+      if (reportName === 'tagPeople') {
+        apiEndpoint = apiStart + `tags/${reportWanted.params.tag}/people` + apiEnd;
       }
+
+      fetch( apiEndpoint )
+        .then( resp => resp.json() )
+        .then( json => arr.concat( json.results ))
+        .then( data => Papa.unparse( data ) )
+        .then( csv => {
+          let csvFile = "data:text/csv;charset=utf-8," + csv;
+          this.setState( { [reportName]: csvFile } )
+        } )
+        .catch( error => console.log( "Error: ", error ) )
 
     })
 
