0f354f852107e298de9474af7642ca74defbe039
Beth Urban <beth@mosaicstg.com>
2020-03-09 14:15:21 -0400

    Refactor to functional component

--- src/pages/NationAdd.js
+++ src/pages/NationAdd.js
@@ -1,43 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-class NationAdd extends Component {
+function NationAdd() {
 
-  state = {
-    slug: '',
-    url: ''
+  const [ slug, setSlug ] = useState( '' );
+  const [ url, setUrl ]   = useState( '' );
+
+  const handleChange = e => {
+    setSlug(e.target.value)
   };
 
-  render() {
+  const handleSubmit = () => {
+    fetch( `http://localhost:3001/nations?slug=${slug}`, {
+      method: 'POST'
+    } )
+      .then( resp => resp.json() )
+      .then( json => setUrl(json) )
+  };
 
-    const handleChange = e => {
-      this.setState({ slug: e.target.value })
-    };
+  return (
+    <div>
+      <header>
+        <h1>Add a new nation to the tool</h1>
+      </header>
 
-    const handleSubmit = () => {
-      fetch(`http://localhost:3001/nations?slug=${this.state.slug}`, {
-        method: 'POST'
-      })
-        .then(resp => resp.json())
-        .then(json => this.setState( { url: json }) )
-    };
+      <Form onSubmit={e => {
+        e.preventDefault();
+        handleSubmit()
+      }}>
+        <Form.Control size='sm' type='text' placeholder="Enter the nation's slug" onChange={handleChange}/>
+        <Button type="submit">Request authorization from nation (you will need to log in)</Button>
+      </Form>
 
-    return (
-      <div>
-        <header>
-          <h1>Add a new nation to the tool</h1>
-        </header>
+      {url.length > 0 ? <Button href={url}>Register nation for reports</Button> : null}
 
-        <Form onSubmit={e => { e.preventDefault(); handleSubmit() }}>
-          <Form.Control size='sm' type='text' placeholder="Enter the nation's slug" onChange={handleChange} />
-          <Button type="submit">Request authorization from nation (you will need to log in)</Button>
-        </Form>
+    </div>
+  )
 
-        { this.state.url.length > 0 ? <Button href={this.state.url}>Register nation for reports</Button> : null }
-
-      </div>
-    )
-  }
 }
 
 export default NationAdd;
